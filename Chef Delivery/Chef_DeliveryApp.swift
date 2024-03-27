5f34429ed5e86d357fdccf13e9971e1f7c7c029f
Uendell Magno <uendellmagno@gmail.com>
2024-03-27 12:40:40 -0300

    Initial Commit

---
+++ Chef Delivery/Chef_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_DeliveryApp.swift
+//  Chef Delivery
+//
+//  Created by Uendell Magno on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Chef_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
